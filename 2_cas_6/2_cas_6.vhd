0ab7eeb7e673de34500e6543f16ca435ca39b60c
penenadpi <penenadpi@gmail.com>
2022-10-31 07:00:48 -0400

    Add files via upload

--- 2_cas_6/2_cas_6.vhd
+++ 2_cas_6/2_cas_6.vhd
@@ -1,3 +1,4 @@
+
 entity demux_1to8 is
 port (A : in bit;
 Sel : in bit_vector (2 downto 0);
@@ -27,21 +28,24 @@ entity demux_1to8_TB is
 end entity;
 
 architecture demux_1to8_TB_arch of demux_1to8_TB is
-	component demux_1to8
-		port (A : in bit_vector(2 downto 0);
-		F: out bit_vector(7 downto 0));
-	end component;
-	signal A_TB : bit_vector(2 downto 0);
+component demux_1to8 is
+port (A : in bit;
+Sel : in bit_vector (2 downto 0);
+F : out bit_vector (7 downto 0) );
+end component;
+
+	signal A_TB : bit;
+	signal Sel_TB :  bit_vector(2 downto 0);
 	signal F_TB :  bit_vector(7 downto 0);
 	
 begin
-	DUT1: demux_1to8 port map (A => A_TB, F => F_TB);
+	DUT1: demux_1to8 port map (A => A_TB, Sel=> Sel_TB, F => F_TB);
 
 	STIMULUS : process
 
 	begin
-		A_TB <= "001"; wait for 100 ps;
-		A_TB <= "010"; wait for 100 ps;
+		A_TB <= '1'; Sel_TB <= "010"; wait for 100 ps;
+		A_TB <= '1'; Sel_TB <= "111"; wait for 100 ps;
 	end process; 
 
 end architecture;
